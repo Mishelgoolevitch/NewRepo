3b1972c38d2046433f5032ad2cc9a349031b6b53
Mishelgoolevitch <118799314+Mishelgoolevitch@users.noreply.github.com>
2024-10-26 20:05:17 +0300

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CarReduserBlazer.Data;
+namespace CarReduserBlazer.Data
+{
+   
+    using Microsoft.EntityFrameworkCore;
+    using CarReduserBlazer.Data;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Reducer> Reducers { get; set; }
+    }
+
+    public class Reducer
+    {
+        public int Id { get; set; } // Добавьте идентификатор
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public int NumberOfWheels { get; set; }
+        public double GearRatio { get; set; }
+        public double InputTorque { get; set; }
+        public double OutputTorque { get; set; }
+    }
+}
---
+++ Migrations/20240918053504_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarReduserBlazer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reducers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfWheels = table.Column<int>(type: "int", nullable: false),
+                    GearRatio = table.Column<double>(type: "float", nullable: false),
+                    InputTorque = table.Column<double>(type: "float", nullable: false),
+                    OutputTorque = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reducers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reducers");
+        }
+    }
+}
