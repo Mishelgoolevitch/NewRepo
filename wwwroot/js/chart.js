dee48a06ff57de04bd882f8b9c42d95d0115cb64
Mishelgoolevitch <118799314+Mishelgoolevitch@users.noreply.github.com>
2024-10-26 20:12:43 +0300

    Add files via upload

---
+++ wwwroot/js/chart.js
@@ -0,0 +1,26 @@
+﻿window.createProductChart = (products) => {
+    const ctx = document.getElementById('productChart').getContext('2d');
+    const labels = products.map(p => p.name);
+    const data = products.map(p => p.price);
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Цены продуктов',
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+};
---
+++ wwwroot/js/download.js
@@ -0,0 +1,10 @@
+﻿window.downloadFile = (fileName, streamReference) => {
+    const url = URL.createObjectURL(streamReference);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
+};
